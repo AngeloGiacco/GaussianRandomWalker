e8879e5a114e68e25394c3992a14e1c020ee9d5b
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-21 10:47:42 +0000

    create reset function and sliders

--- sketch.js
+++ sketch.js
@@ -1,28 +1,13 @@
 var walkers = [];
-var mean = 0;
-var sd = 1;
+var meanX;
+var meanY;
+var sd;
 
-function Walker(mean,sd) {
-  this.mean = mean;
-  this.sd = sd;
-  this.x = window.innerWidth/2;
-  this.y = window.innerHeight/2;
-  var stepy;
-  var stepx;
-
-  this.walk = function() {
-    stepx = generator.nextGaussian() * this.sd + this.mean;
-    stepy = generator.nextGaussian() * this.sd + this.mean;
-    this.x += stepx;
-    this.y += stepy;
-  }
-
-  this.render = function() {
-    stroke(255);
-    strokeWeight(2);
-    point(this.x,this.y);
-  }
-}
+let sdSlider;
+let meanSlider;
+let xSlider;
+let ySlider;
+let strokeWeightSlider;
 
 function setup() {
   createCanvas(
@@ -30,14 +15,71 @@ function setup() {
     window.innerHeight
   );
   background(0);
-  var walker = new Walker(mean,sd);
+  sdSlider = createSlider(0, 10, 2, 0);
+  sdSlider.position(20, 20);
+  meanXSlider = createSlider(-1, 1, 0, 0);
+  meanXSlider.position(20, 50);
+  meanYSlider = createSlider(-1, 1, 0, 0);
+  meanYSlider.position(20, 80);
+  xSlider = createSlider(0, width, width/2, 0);
+  xSlider.position(20, 110);
+  ySlider = createSlider(0, height, height/2, 0);
+  ySlider.position(20, 140);
+  strokeWeightSlider = createSlider(1,4,2,0);
+  strokeWeightSlider.position(20,170);
+  strokeWeight(0);
+  text('Standard Deviation', sdSlider.x * 2 + sdSlider.width, 35);
+  fill(255);
+  text('Mean X', meanXSlider.x * 2 + meanXSlider.width, 65);
+  fill(255);
+  text('Mean Y', meanYSlider.x * 2 + meanYSlider.width, 95);
+  fill(255);
+  text('Starting X', xSlider.x * 2 + xSlider.width, 125);
+  fill(255);
+  text('Starting Y', ySlider.x * 2 + ySlider.width, 155);
+  fill(255);
+  text('Stroke Weight', strokeWeightSlider.x * 2 + strokeWeightSlider.width, 185);
+  fill(255);
+  button = createButton('reset');
+  button.position(20, 195);
+  button.mousePressed(reset);
+  meanX = meanXSlider.value();
+  meanY = meanYSlider.value();
+  sd = sdSlider.value();
+
+  var walker = new Walker(meanX,meanY,sd);
   walkers.push(walker);
 }
 
 function draw() {
-  console.log(walkers);
   walkers.forEach(function(walker) {
     walker.render();
     walker.walk();
   });
+
+}
+
+function reset() {
+  background(0);
+  walkers = [];
+  meanX = meanXSlider.value();
+  meanY = meanYSlider.value();
+  sd = sdSlider.value();
+  var walker = new Walker(meanX,meanY,sd);
+  walkers.push(walker);
+  strokeWeight(0);
+  text('Standard Deviation', sdSlider.x * 2 + sdSlider.width, 35);
+  fill(255);
+  text('MeanX', meanXSlider.x * 2 + meanXSlider.width, 65);
+  fill(255);
+  text('MeanY', meanYSlider.x * 2 + meanYSlider.width, 95);
+  fill(255);
+  text('Starting X', xSlider.x * 2 + xSlider.width, 125);
+  fill(255);
+  text('Starting Y', ySlider.x * 2 + ySlider.width, 155);
+  fill(255);
+  text('Stroke Weight', strokeWeightSlider.x * 2 + strokeWeightSlider.width, 185);
+  fill(255);
+  var sw = strokeWeightSlider.value();
+  strokeWeight(sw);
 }
